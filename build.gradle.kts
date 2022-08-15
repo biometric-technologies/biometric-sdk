bf8b83234e295821ab76d8b24c990eeec5fbb18b
Slava Gornostal <slava.gornostal@gmail.com>
2022-08-15 22:36:56 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("multiplatform") version "1.7.10"
+}
+
+group = "net.iriscan"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "17"
+        }
+        withJava()
+    }
+    js(BOTH) {
+        browser {
+        }
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "biometric-sdk"
+
